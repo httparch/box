63581265898fa2909fd63f13c894c8f0d619a76b
httparch <shanebigornia123@gmail.com>
2025-02-04 00:21:40 +0800

    added method for cart quantity and set localstorage

--- amazon-clone/scripts/cart.js
+++ amazon-clone/scripts/cart.js
@@ -1,19 +1,29 @@
-export let cart = [
-    {
+
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+    cart = [{
+        productId: 0,
+        quantity:1
+    },{
         productId: 1,
         quantity:1
-    },
-    {
-        productId: 2,
-        quantity:1
-    }
-];
+    }]
+}
+
+function reset(){
+    localStorage.removeItem('cart')
+}
+
+function saveStorage(){
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(itemId){
     let matchingItem;
         
         cart.forEach((item) =>{
-            if(itemId == item.productId){
+            if(itemId.productId == item.productId){
                 matchingItem = item;
             }
         })
@@ -22,22 +32,28 @@ export function addToCart(itemId){
             matchingItem.quantity += 1;
         }else{
             cart.push({
-                productId: itemId,
+                productId: Number(itemId.productId),
                 quantity: 1
             });
         }
-
+        console.log(cart)
+        saveStorage();
+        //reset();
 }
 
 export function removeToCart(itemId){
     const newCart = [];
-
+    
     cart.forEach(item => {
-        if(item.productId !== itemId){
+        if(item.productId != itemId){//item.product is a number and itemId is a string, di gumagana kasi naka strict operator .
             newCart.push(item);
         }
     })
-
+    
     cart = newCart;
+    saveStorage();
+}
 
+export function cartQuantity(){
+    return cart.length > 1 ? `${cart.length} items` : `${cart.length} item`;
 }
